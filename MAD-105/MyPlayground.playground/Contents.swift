34fd075f6dc3c84e274da48c1ef3b0c446c24d44
Starman02 <devingrischow2002@gmail.com>
2022-08-22 11:18:45 -0500

    oh my thats alot of content

---
+++ MAD-105/MyPlayground.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+var greeting = "hello world!"
+print(greeting)
+
+var name = "Devin"
+var last_name = " grischow "
+
+var switft_enjoyer = "Finds doing switft enjoyable "
+
+print(name + last_name + switft_enjoyer + "!")
+
+
+var message2 = "\(name), welcome to the world of switft"
+print(message2)
+
+var the_terminator = "hi my name is carmen winston"
+print(the_terminator , name, terminator: "")
+
+var to_the_line = "how do you do?"
+var line = ""
+print(to_the_line, name, to:&line)
+print(line)
